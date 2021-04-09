13abd7cf626f995f180dd7a4153dd0b8bcde61b4
Iskren <Iskren.1990@abv.bg>
2021-04-09 20:40:30 +0300

    feat/error logging in file

--- controllers/error.js
+++ controllers/error.js
@@ -1,19 +1,20 @@
 const fs = require("fs");
-const filePath = require("path").resolve(__dirname + "/../logs/error.txt");
-// const filePath = path.resolve(__dirname + "/../logs/error.txt");
-const stream = fs.createWriteStream(filePath, { 'flags': 'a' , 'encoding': null , 'mode': 0666 });
+// heroku does not like files
+const path = require("path");
+const filePath = path.resolve(__dirname + "/../logs/error.txt");
+// const stream = fs.createWriteStream(filePath, { 'flags': 'a' , 'encoding': null , 'mode': 0666 });
 
 const error = {
     post: async (req, res, next) => {
         
         try {
             const  errorData = JSON.stringify(req.body);
-            stream.write(`[ ${new Date().toLocaleString("bg-BG")} ] : `+ errorData + "\n");
-            res.end();
+            // stream.write(`[ ${new Date().toLocaleString("bg-BG")} ] : `+ errorData + "\n");
+            res.status(200).json({written: "true"});
         } catch (error) {
             console.log(error);
             // global error handler here
-            res.end();
+            res.status(500).json({written: "false"});
         }
     }
 }
